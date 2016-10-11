72a6701232c2d83657859c0e377e1a9c53780a64
Roman <roman@riflmedia.com>
2016-10-11 17:00:36 +0300

    add gulp

---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by molot on 11/10/2016.
+ */
+
+angular.module('chat', []);
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+/**
+ * Created by molot on 11/10/2016.
+ */
+
+var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+//server
+
+gulp.task('start', function() {
+    gulp.src('app')
+        .pipe(server({
+            livereload: true,
+            open: true
+        }));
+});
+
+//styles
+
+gulp.task('style', function () {
+    return gulp.src('app/sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 15 versions']
+        }))
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/sass/**/*.sass', ['style']);
+});
+
+gulp.task('default',['start','watch']);
+
+
