234ff28a02123d6402f723716e2464f3b853687a
Roman <roman@riflmedia.com>
2016-10-19 14:54:31 +0300

    update

--- app/js/controllers/chat.controller.js
+++ app/js/controllers/chat.controller.js
@@ -2,11 +2,46 @@
  * Created by molot on 11/10/2016.
  */
 
-function chatCtrl() {
+angular.module('chat')
+    .controller('ChatCtrl', ['chatService', '$firebaseAuth', chatCtrl]);
+
+function chatCtrl(chatService, $firebaseAuth){
     var vm = this;
-    vm.test = 'blya'
+    var auth = $firebaseAuth();
+
+    vm.messages = chatService.getMessages();
+    vm.messagesToShow = chatService.getMessagesToShow();
+
+    vm.addMessage = function(){
+        if( vm.author != null ){
+            var message = {
+                authorId: vm.author.uid,
+                author: vm.author.displayName,
+                authorImg: vm.author.photoURL,
+                message: vm.newMessage
+            }
+        }else{
+            alert('Санала зарегистрируйтесь');
+        }
+        if( vm.newMessage != '' ){
+            chatService.addMessage(message);
+            vm.newMessage = '';
+        }else{
+            alert('Введите сообщение')
+        }
+    }
+
+    vm.login = function(){
+        auth.$signInWithPopup('google');
+    };
+    vm.logout = function(){
+        auth.$signOut();
+    };
+
+    auth.$onAuthStateChanged(function(authData) {
+        vm.author = authData;
+    });
+
 }
 
 
-angular.module('chat')
-    .controller('chatCtrl',[chatCtrl])
